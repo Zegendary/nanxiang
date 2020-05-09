29aab2f9c67484528a4cd93116e03f1f65cefcfe
Zegendary <zxw1992513@qq.com>
2020-05-09 22:02:51 +0800

    调整代码

--- app.js
+++ app.js
@@ -19,22 +19,28 @@ app.get('/', function(req, res) {
 });
 
 app.get('/like', async (req, res) => {
-  const data = await Like.findAndCountAll({
-    where: {target: req.query.target}
+  const rank = await Rank.findOne({
+    where: {phone: req.query.target}
   })
   res.render('pages/like', {
     target: req.query.target,
-    count: data.count
+    count: rank && rank.dataValues && rank.dataValues.likeCount
   });
 });
 
 app.get('/share', async (req, res) => {
-  const data = await Like.findAndCountAll({
-    where: {creator: req.query.creator}
+  const rank = await Rank.findOne({
+    where: {phone: req.query.creator}
   })
+  const result = await Rank.findAll({
+    limit: 10 ,
+    order: [[Sequelize.col('likeCount'),'DESC']]
+  })
+  console.log(result)
   res.render('pages/share', {
     creator: req.query.creator,
-    count: data.count
+    rank: rank.dataValues,
+    ranks: result,
   });
 });
 
@@ -46,18 +52,18 @@ app.post('/like', async (req, res) => {
   if(likes.length === 0){
     try{
       await Like.create({target: req.body.target, creator: req.body.creator})
-      const data = await Like.findAndCountAll({
-        where: {target: req.body.target}
-      })
-      await Rank.findOne({
+      const rank = await Rank.findOne({
         where: {phone: req.body.target}
-      }).then((rank) => {
-        if (!rank) {
-          return Rank.create({phone: req.body.target})
-        }
-        Rank.update({likeCount: rank.likeCount ++},{phone: req.body.target})
       })
-      res.send({count: data.count})
+      if (rank && rank.dataValues && rank.dataValues.id) {
+        Rank.update({likeCount: rank.dataValues.likeCount++},{
+          where: {phone: req.body.target}
+        })
+        res.send({count: rank.dataValues.likeCount})
+      }else{
+        Rank.create({phone: req.body.target})
+        res.send({count: 1})
+      }
     }catch (e) {
       res.statusCode = 400;
       res.send(e)
